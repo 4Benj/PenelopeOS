e496c2f17a86c7865f771ea5309d494e2c4cb4ca
Benj <benjamin7006@gmail.com>
2024-06-14 23:26:56 +0800

    Proper VGA Buffer Driver

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello world!";
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -15,15 +15,11 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    println!("Hello World{}", "!");
+    println!("This is a test of the VGA buffer driver{}", "!");
+    print!("on Penelope OS v0.1.0");
+    print!(" written in Rust!");
 
     // we do nothing here
     loop {}
