f8edc1716f87883d5b7fa7e83298355098fecf3a
Benj <benjamin7006@gmail.com>
2024-06-15 23:25:06 +0800

    Optimize the shell to use dynamic memory

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,47 +1,58 @@
 use crate::{drivers::keyboard::read_key, hlt_loop, print, println, serial_println};
 use pc_keyboard::DecodedKey;
-
-const MAX_INPUT_SIZE: usize = 256;
+use alloc::string::String;
+use alloc::vec::Vec;
 
 pub fn shell() {
     serial_println!("[Shell] Initializing");
     loop {
         print!("> ");
-        let mut buffer = [0u8; MAX_INPUT_SIZE];
-        let input_length = read_line(&mut buffer);
-        if input_length == 0 {
-            continue;
+        let command = read_line();
+        let mut args = command.split_whitespace();
+        if let Some(cmd) = args.next() {
+            match cmd {
+                "echo" => {
+                    let output: String = args.collect::<Vec<&str>>().join(" ");
+                    println!("{}", output);
+                }
+                "exit" => {
+                    println!("Exiting shell.");
+                    break;
+                }
+                "panik" => {
+                    // Trigger a panic
+                    panic!("You triggered a panic!");
+                }
+                _ => {
+                    println!("Unknown command: {}", cmd);
+                }
+            }
         }
-        let command = core::str::from_utf8(&buffer[..input_length]).unwrap_or("");
-        execute_command(command);
     }
     serial_println!("[Shell] Ending");
 }
 
-fn read_line(buffer: &mut [u8]) -> usize {
+fn read_line() -> String  {
     serial_println!("[Shell] Reading Line");
-    let mut index = 0;
+    let mut input = String::new();
     loop {
         if let Some(key) = read_key() {
             match key {
-                DecodedKey::Unicode(character) => match character {
+                DecodedKey::Unicode(c) => match c {
                     '\n' => {
                         println!();
                         break;
                     }
                     '\u{8}' => {
-                        if index > 0 {
-                            index -= 1;
+                        if input.len() > 0 {
                             print!("\u{8} \u{8}");
+                            input.pop();
                         }
                         continue;
                     }
                     _ => {
-                        if index < buffer.len() {
-                            buffer[index] = character as u8;
-                            index += 1;
-                            print!("{}", character);
-                        }
+                        input.push(c);
+                        print!("{}", c);
                     }
                 },
                 _ => {}
@@ -50,32 +61,5 @@ fn read_line(buffer: &mut [u8]) -> usize {
             x86_64::instructions::hlt();
         }
     }
-    index
-}
-
-fn execute_command(command: &str) {
-    serial_println!("[Shell] Executing command: {}", command);
-
-    let mut parts = command.split_whitespace();
-    if let Some(cmd) = parts.next() {
-        match cmd {
-            "echo" => {
-                for arg in parts {
-                    print!("{} ", arg);
-                }
-                println!();
-            }
-            "exit" => {
-                println!("Exiting shell.");
-                // Handle exit logic, if needed
-            }
-            "panik" => {
-                // Trigger a panic
-                panic!("You triggered a panic!");
-            }
-            _ => {
-                println!("Unknown command: {}", cmd);
-            }
-        }
-    }
+    input
 }
